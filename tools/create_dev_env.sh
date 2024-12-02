9f506042d2513e447e7782bc9d018f9efafbde03
Aleksi Hyppönen <39343881+jehawb@users.noreply.github.com>
2024-12-02 14:17:30 +0200

    Delete tools/create_dev_env.sh

--- tools/create_dev_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the directory of the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Run the Python files
-python3 "$SCRIPT_DIR/setup_cli.py"
-python3 "$SCRIPT_DIR/fork_repo.py"
