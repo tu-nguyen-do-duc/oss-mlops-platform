17690e447137d445cebbce9bb751cc71689040a6
Alex Kiippa <ext-alex.1.kiippa@sok.fi>
2024-12-04 11:30:47 +0200

    Add looping for the setup if any other than the last option is chosen from the cli tool

--- tools/CLI-tool/fork_repo.py
+++ tools/CLI-tool/fork_repo.py
@@ -18,36 +18,6 @@ def main(repo_name: str, org_name: str):
     """
     fork_repo(repo_name, org_name)
 
-#     if repo_owner:
-#         print("Forking the repository...")
-#         fork_repo(repo_name, repo_owner)
-#     else:
-#         print("Could not fetch repository information. Exiting...")
-
-# def get_repo_owner(repo_name: str):
-#     """Fetch the repository owner using GitHub API."""
-#     result = subprocess.run(
-#         f"gh api -X GET search/repositories -f q='{repo_name} in:name' --jq '.items[] | {{name, owner: .owner.login}}'",
-#         shell=True,
-#         capture_output=True,
-#         text=True
-#     )
-
-#     if result.returncode != 0:
-#         print("Error fetching repository information:", result.stderr)
-#         return None
-    
-#     try:
-#         print(result.stdout)
-#         repo_info_list = [json.loads(line) for line in result.stdout.strip().split('\n')]
-#         repo_info = repo_info_list[0]
-#         print(f"Repository found: {repo_info}")
-#         owner_name = repo_info['owner']
-#         return owner_name
-#     except json.JSONDecodeError:
-#         print("Error decoding repository information")
-#         return None
-
 def fork_repo(repo_name: str, org_name):
     """Fork the repository using GitHub CLI."""
     working_repo_name = typer.prompt("Enter unique name for your working repository", type=str)
@@ -63,6 +33,8 @@ def fork_repo(repo_name: str, org_name):
         subprocess.run(["git", "checkout", "-b", "production", "origin/production"])
         subprocess.run(["git", "checkout", "development"])
         os.chdir("../")
+
+        # This option was for the older versions of GH in order to clone the forked repo
 
     # if sys.platform == "darwin":
     #     subprocess.run(f'gh repo fork {owner}/{repo_name} --clone --fork-name "{working_repo_name}" --org {owner}', shell=True)
--- tools/CLI-tool/setupCLITool.sh
+++ tools/CLI-tool/setupCLITool.sh
@@ -9,33 +9,40 @@ read -p "Enter the organization name: " org_name
 
 read -p "Enter the name of the config repo: " repo_name
 
-echo "Which script(s) would you like to run?"
-echo "1) Configure Github (GH) for the tool (configure_gh.py)"
-echo "2) Create configuration repo for one or more ML project working repos (setup_cli.py)"
-echo "3) Create one ML project working repo based on a configuration repo (fork_repo.py)"
-echo "4) Both (step 3 is based on config repo created in step 2)"
-read -p "Enter your choice (1/2/3/4): " choice
+while true; do
+    echo "Select an option:"
+    echo "Which script(s) would you like to run?"
+    echo "1) Configure Github (GH) for the tool (configure_gh.py)"
+    echo "2) Create configuration repo for one or more ML project working repos (setup_cli.py)"
+    echo "3) Create one ML project working repo based on a configuration repo (fork_repo.py)"
+    echo "4) Both (step 3 is based on config repo created in step 2)"
+    echo "5) Exit"
+    read -p "Enter your choice: " choice
 
-# Execute the selected script(s)
-case $choice in
-    1)
-        python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
-        ;;
-    2)
-        python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
-        python3 oss-mlops-platform/tools/CLI-tool/setup_cli.py "$repo_name" "$org_name"
-        ;;
-    3)
-        python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
-        python3 oss-mlops-platform/tools/CLI-tool/fork_repo.py "$repo_name" "$org_name"
-        ;;
-    4)
-        python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
-        python3 oss-mlops-platform/tools/CLI-tool/setup_cli.py "$repo_name" "$org_name"
-        python3 oss-mlops-platform/tools/CLI-tool/fork_repo.py "$repo_name" "$org_name"
-        ;;
-    *)
-        echo "Invalid choice. Exiting."
-        exit 1
-        ;;
-esac
+    case $choice in
+        1)
+            python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
+            ;;
+        2)
+            python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
+            python3 oss-mlops-platform/tools/CLI-tool/setup_cli.py "$repo_name" "$org_name"
+            ;;
+        3)
+            python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
+            python3 oss-mlops-platform/tools/CLI-tool/fork_repo.py "$repo_name" "$org_name"
+            ;;
+        4)
+            python3 oss-mlops-platform/tools/CLI-tool/configure_gh.py
+            python3 oss-mlops-platform/tools/CLI-tool/setup_cli.py "$repo_name" "$org_name"
+            python3 oss-mlops-platform/tools/CLI-tool/fork_repo.py "$repo_name" "$org_name"
+            exit 0
+            ;;
+        5)
+            echo "Exiting. Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            ;;
+    esac
+done
