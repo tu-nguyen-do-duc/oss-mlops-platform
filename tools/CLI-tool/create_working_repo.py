75b2e21181b42191441a492f6a0e9fd6c065f628
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2025-03-28 12:25:18 +0200

    Refactoring

--- tools/CLI-tool/create_working_repo.py
+++ tools/CLI-tool/create_working_repo.py
@@ -10,6 +10,7 @@ import typer
 # Define the Typer app
 app = typer.Typer()
 
+
 # Use Typer to define repo_name as an argument
 @app.command()
 def main(repo_name: str, org_name: str):
@@ -17,6 +18,20 @@ def main(repo_name: str, org_name: str):
     Main function to fetch repo details and fork it.
     """
     fork_repo(repo_name, org_name)
+
+
+def get_working_repo_name(config_repo_name: str):
+    if config_repo_name.startswith("Config-"):
+        as_split = config_repo_name.split("-")
+        as_split[0] = "Working"
+        default_working_repo_name = "-".join(as_split)
+        return typer.prompt(
+            "Enter unique name for your working repository:",
+            type=str,
+            default=default_working_repo_name,
+        )
+    return typer.prompt("Enter unique name for your working repository:", type=str)
+
 
 def fork_repo(repo_name: str, org_name):
     """Fork the repository using GitHub CLI."""
@@ -27,13 +42,23 @@ def fork_repo(repo_name: str, org_name):
         as_split[0] = "Working"
         default_working_repo_name = "-".join(as_split)
 
-    working_repo_name = typer.prompt("Enter unique name for your working repository:", type=str, default=default_working_repo_name)    
+    working_repo_name = typer.prompt(
+        "Enter unique name for your working repository:",
+        type=str,
+        default=default_working_repo_name,
+    )
     version = subprocess.run(["gh", "--version"], capture_output=True, text=True)
 
     if "2.4.0" in version.stdout:
-        subprocess.run(f'gh repo fork {org_name}/{repo_name} --clone --remote-name {working_repo_name} --org {org_name}', shell=True)
+        subprocess.run(
+            f"gh repo fork {org_name}/{repo_name} --clone --remote-name {working_repo_name} --org {org_name}",
+            shell=True,
+        )
     else:
-        subprocess.run(f'gh repo fork {org_name}/{repo_name} --clone --fork-name "{working_repo_name}" --org {org_name}', shell=True)
+        subprocess.run(
+            f'gh repo fork {org_name}/{repo_name} --clone --fork-name "{working_repo_name}" --org {org_name}',
+            shell=True,
+        )
         os.chdir(working_repo_name)
         subprocess.run(["git", "checkout", "-b", "staging", "origin/staging"])
         subprocess.run(["git", "checkout", "-b", "production", "origin/production"])
